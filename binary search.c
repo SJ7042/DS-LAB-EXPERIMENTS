f77703adae977f9ace555acb47545925df5536f1
HARSHINI S J <yagavasj1234@gmail.com>
2026-01-09 15:03:17 +0530

    Update binary search.c

--- binary search.c
+++ binary search.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
 
-int binarySearch(int arr[], int size, int target) {
-    int left = 0;
-    int right = size - 1;
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int target = 7;
+    int left = 0, right = 4;
 
     while (left <= right) {
-        int mid = left + (right - left) / 2;
+        int mid = (left + right) / 2;
 
         if (arr[mid] == target) {
-            return mid;         
+            printf("Found at index %d\n", mid);
+            return 0;
         } else if (arr[mid] < target) {
-            left = mid + 1;    
+            left = mid + 1;
         } else {
-            right = mid - 1;    
+            right = mid - 1;
         }
     }
 
-    return -1;  
-
-int main() {
-    int arr[] = {2, 4, 6, 8, 10, 12};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int target = 8;
-
-    int result = binarySearch(arr, size, target);
-
-    if (result != -1)
-        printf("Element found at index %d\n", result);
-    else
-        printf("Element not found\n");
-
+    printf("Not found\n");
     return 0;
 }
+
